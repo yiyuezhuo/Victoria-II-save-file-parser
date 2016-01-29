a909c704cfd5efad481d04bc8b00d1801a6c7642
yiyuezhuo <616271835@163.com>
2016-01-30 00:36:20 +0800

    add auxiliary function

--- analyse.py
+++ analyse.py
@@ -7,7 +7,9 @@ Created on Fri Jan 29 16:49:09 2016
 import os
 import re
 from select2 import parse
-from collections import defaultdict
+#from collections import defaultdict
+import numpy as np
+import matplotlib.pylab as plt
 
 
 def sample():
@@ -58,6 +60,7 @@ def test2(al):
         weight_keys=['literacy']
         #dd=defaultdict(lambda:{'size':0.0,'money':0.0})
         sum_dict={mk:{} for mk in main_keys}
+        #sum_dict['sum']={wk:0.0 for wk in wait_keys}#sum是直接和，不分类
         record={'pop_number':len(rd['pop'].values()),'sum':sum_dict}
         for pop in rd['pop'].values():
             for mk in main_keys:
@@ -69,11 +72,66 @@ def test2(al):
                 for wk in weight_keys:
                     record['sum'][mk][tk][wk]+=float(pop[wk])*float(pop['size'])
                     #record['sum'][mk][wk]+=float(pop[wk])
+        sur_l=record['sum']['type'].values()
+        record['sum']['sum']={sk:sum([sd[sk] for sd in sur_l]) for sk in sur_l[0].keys()}
         record['date']=a['date']
         seq.append(record)
         print 'deal '+a['path']+' succ'
     return seq
-        
-        
+    
+def T(seq,main_key,sub_key,key):
+    #选择出一个序列 如 'type' 'farmer' 'money'
+    return [record['sum'][main_key][sub_key][key] for record in seq]
+    
+def report_detail(seq,main_key,sk,key1,key2):
+    axis=[int(record['date'][0]) for record in seq]
+    l1=np.array(T(seq,main_key,sk,key1))
+    l2=np.array(T(seq,main_key,sk,key2))
+    plt.plot(axis,l1/l2)
+    plt.show()
+    
+def report_p(seq,main_key,key1,key2):
+    sub_keys=seq[0]['sum'][main_key].keys()
+    axis=[int(record['date'][0]) for record in seq]
+    for sk in sub_keys:
+        print 'plot '+main_key +' '+sk+' '+key1+'/'+key2
+        l1=np.array(T(seq,main_key,sk,key1))
+        l2=np.array(T(seq,main_key,sk,key2))
+        plt.plot(axis,l1/l2)
+        plt.show()
+        print ' '
+def percent_flow(seq,main_key,detail=None):
+    sub_keys=seq[0]['sum'][main_key].keys()
+    d={sk:T(seq,main_key,sk,'size') for sk in sub_keys}
+    ds=np.array([sum([sl[i] for sl in d.values()]) for i in range(len(d.values()[0]))])
+    axis=[int(record['date'][0]) for record in seq]
+    if detail==None:
+        for key,value in d.items():
+            y=np.array(value)/ds
+            plt.plot(axis,y,label=key)
+    else:
+        y=np.array(d[detail])/ds
+        plt.plot(axis,y,label=detail)
+    plt.legend()
+    plt.show()
+    
+def report_par(seq,main_key,skl,key1,key2,loc=2):
+    axis=[int(record['date'][0]) for record in seq]
+    for sk in skl:
+        l1=np.array(T(seq,main_key,sk,key1))
+        l2=np.array(T(seq,main_key,sk,key2))
+        plt.plot(axis,l1/l2,label=sk)
+    plt.legend(loc=loc)
+    plt.show()
+
+def report(seq):
+    report_p(seq,'type','money','size')
+    report_p(seq,'type','literacy','size')
+    report_p(seq,'religion','money','size')
+    report_p(seq,'religion','literacy','size')
+    
+type_l=['farmers', 'bureaucrats', 'capitalists', 'artisans', 'aristocrats', 'officers', 'clergymen', 'labourers', 'slaves', 'clerks', 'soldiers', 'craftsmen']
+religion_l=['shiite', 'catholic', 'mahayana', 'theravada', 'animist', 'orthodox', 'jewish', 'hindu', 'protestant', 'gelugpa', 'shinto', 'coptic', 'sunni', 'sikh']
+
 al=sample()
-seq=test2(al[:3])
+#seq=test2(al[:3])
